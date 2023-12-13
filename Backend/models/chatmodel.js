35d2c85a4f058e083c9d70b9661fcf1fb3bd4a89
ShrutiKhanal <shruti.khanal2002@gmail.com>
2023-12-13 17:04:08 +0545

    chat progress

---
+++ Backend/models/chatmodel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const chatModel = mongoose.Schema(
+  {
+    chatName: { type: String, trim: true },
+    users: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    latestmessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Chat = mongoose.model("Chat", chatModel);
+
+module.exports = Chat;
+
+// chatName
+// users
+// latestMessage
